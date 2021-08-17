196df534887666eca8f98d32ce71cd505d231488
fessouma1 <80673653+fessouma1@users.noreply.github.com>
2021-08-17 14:38:43 -0400

    the comparison
    add 2 number to find a specific value

---
+++ app.js
@@ -0,0 +1,20 @@
+let twoSum = function(nums, target) {
+    let index = 0;
+    let compare = 1;
+    for (let i = 0; i <= nums.length; i++)
+        {
+           for (let j = 1; j <= nums.length; j++)
+            {
+                if(nums[i] + nums[j] === target)
+                    {
+                        console.log(`[ ${i} , ${j} ]`);
+                    }
+            }
+            
+        }
+    
+}
+
+let nums = [2,7,11,15];
+let target = 9;
+ twoSum (nums, target);
